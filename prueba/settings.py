a1680bc3631af69f74840240338c27e580ab1ff7
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-12-06 20:05:11 -0300

    dotenv

--- prueba/settings.py
+++ prueba/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import dj_database_url
 import django_heroku
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k$i0wj87krplt7wlmx^f+1*bhx#l86twcao!uw&baf^oiw$q0e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
