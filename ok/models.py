7a9b05d2eb2bf50144099a7941f9d1a664b29333
Cristian D Biancotti <cdbiancotti@gmail.com>
2021-12-06 00:10:16 -0300

    migrations

--- ok/models.py
+++ ok/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Perro(models.Model):
+    nombre = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return f'Soy {self.nombre}'  
--- ok/views.py
+++ ok/views.py
@@ -1,7 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Perro
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Anda pibe! anda!</h1>')
+    next_dog = Perro.objects.count()
+    perro = Perro.objects.create(nombre=f'perro {next_dog}')
+    return HttpResponse(f'{perro}')
